7d47071544d2caf45bf07784702ed0e10dcf18da
Aaron Smith <smitham50@gmail.com>
2020-10-01 12:56:59 -0400

    added react-router-dom, tweaked navbar and changed app to class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Navigation from './Components/Navigation'
+import Navigation from './Components/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation></Navigation>
-      <header className="App-header">
-        Header
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation></Navigation>
+        <header className="App-header">
+          Header
       </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
