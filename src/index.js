8469183fd56cc3731d93cb2e5510eb41e1ebb27c
Aaron Smith <smitham50@gmail.com>
2020-12-17 11:15:25 -0500

    fix/gh-pages routing issue

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
