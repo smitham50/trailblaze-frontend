9b35ba1d91aee8f026cde95a82d44a0292558d19
Aaron Smith <smitham50@gmail.com>
2020-12-29 14:06:12 -0500

    fix/switched back to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
