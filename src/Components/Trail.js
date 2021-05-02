6d210abe091a8e1d8f1467a1d09144a12b745a45
Aaron Smith <smitham50@gmail.com>
2021-05-01 20:43:48 -0400

    restore imageRef

--- src/Components/Trail.js
+++ src/Components/Trail.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 const Trail = (props) => { 
     const [spans, setSpans] = useState(0);
 
+    const imageRef = useRef(null);
+
     useEffect(() => {
         try {
             imageRef.current.addEventListener("load", setRowSpans);
