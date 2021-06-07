24af467cb4bfb65733a3b48fc17fee9772fe235c
Aaron Smith <smitham50@gmail.com>
2021-06-07 12:50:57 -0400

    switch navigation and landing to styled components

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
-import '../Stylesheets/Landing.css';
+import styled from 'styled-components';
+
+const Header = styled.h1`
+    font-family: Amatic SC, cursive;
+`;
+
+const Paragraph = styled.p`
+    font-size: .7em;
+    font-family: Source Sans Pro, sans-serif;
+`;
+
+const JumbotronMod = styled(Jumbotron)`
+    background-color: #758ea9;
+    color: white;
+`;
 
 
 const Landing = (props) => {
     return (
-            <Jumbotron className="jumbotron-mod">
-                <h1 className="headline">Welcome to Trailblaze</h1>
-                <p className="subtext">
+            <JumbotronMod>
+                <Header>Welcome to Trailblaze</Header>
+                <Paragraph>
                     If you've ever spent hours researching hikes in range of you because there were too many choices, this app is for you.
                     Tell us how far you're willing to travel and how many miles you want to hike and we'll give you twenty options
                     to choose from. Pick the one you like, get directions, and add it to your hiked trails.
                     Get to the forest without a fuss.
-            </p>
-            </Jumbotron>
+                </Paragraph>
+            </JumbotronMod>
     )
 }
 
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,11 +3,34 @@ import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import '../Stylesheets/Navbar.css';
+import styled from 'styled-components';
+
+const NavWrapper = styled.div`
+    & .navbar-link {
+        text-decoration: none;
+    }
+    & .navbar-dark .navbar-nav .nav-link {
+        color: white;
+        transition: .3s;
+    }
+    & .mr-auto a {
+        margin: 0 1em;
+    }
+`;
+
+const NavbarBrand = styled(Navbar.Brand)`
+    font-size: 1.7em;
+    font-weight: 700;
+    font-family: Balsamiq Sans, cursive;
+`;
+
+const BrandImage = styled.img`
+    height: 50px;
+    width: 50px;
+`;
 
 
 const Navigation = (props) => {
-
     const handleLogout = async () => {
         localStorage.clear();
 
@@ -21,33 +44,36 @@ const Navigation = (props) => {
     };
 
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" fixed="top" className="navbar-mod" collapseOnSelect>
-            <Nav.Link as={Link} eventKey="1" to="/">
-                <Navbar.Brand>
-                        <img src={process.env.PUBLIC_URL + "/noun_Trail_2056927.svg"} alt="brand" width="30px" height="30px" loading="lazy" />   Trailblaze
-                </Navbar.Brand>
-            </Nav.Link>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    {
-                        props.currentUserData.logged_in
-                            ?
-                                <Fragment>
+        <NavWrapper>
+            <Navbar bg="dark" variant="dark" expand="lg" fixed="top" collapseOnSelect>
+                <Nav.Link as={Link} eventKey="1" to="/">
+                    <NavbarBrand>
+                        <BrandImage src={process.env.PUBLIC_URL + "/noun_Trail_2056927.svg"} alt="brand" loading="lazy" />   Trailblaze
+                </NavbarBrand>
+                </Nav.Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        {
+                            props.currentUserData.logged_in
+                                ?
+                                <>
                                     <Nav.Link as={Link} eventKey="2" to="/trailsearch">Find a Trail</Nav.Link>
                                     <Nav.Link as={Link} eventKey="3" to="/myhikes">My Favorite Hikes</Nav.Link>
                                     <Nav.Link as={Link} eventKey="4" to="/account">Account</Nav.Link>
                                     <Nav.Link as={Link} eventKey="5" to="/" onClick={handleLogout}>Logout {props.currentUserData.user.username}</Nav.Link>
-                                </Fragment>
-                            :
-                                <Fragment>
+                                </>
+                                :
+                                <>
                                     <Nav.Link as={Link} eventKey="6" to="/signup">Signup</Nav.Link>
                                     <Nav.Link as={Link} eventKey="7" to="/login">Login</Nav.Link>
-                                </Fragment>
-                    }
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+                                </>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </NavWrapper>
+        
     );
 };
 
