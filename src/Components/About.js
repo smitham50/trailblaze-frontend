feb3f9583a6062a659a9fe23191cbe2838438075
Aaron Smith <smitham50@gmail.com>
2021-07-08 13:15:15 -0400

    add image to about page and media queries

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,20 +4,48 @@ import styled from 'styled-components';
 const AboutContainer = styled.div`
   background: #f5f5f5;
   color: #635d5d;
-  padding: 6rem 3rem;
+  padding: 2rem 3rem;
+  display: flex;
+
+  @media screen and (max-width: 1000px) {
+    flex-direction: column-reverse;
+    margin: 1rem 0;
+  }
 `;
 
 const Description = styled.p`
   font-size: 1rem;
   margin: 0 3rem;
+  width: auto;
+  padding: 3rem;
+
+  @media screen and (max-width: 1000px) {
+    padding: 1.5rem 0;
+  }
+`;
+
+const AboutImage = styled.img`
+  width: 55%;
+
+  @media screen and (max-width: 1000px) {
+    width: 100%;
+  }
 `;
 
 const About = () => {
   return (
     <AboutContainer>
       <Description>
-        Trailblaze is a location based trail finding application. Plug in how far you're willing to travel and how many miles you'd like to hike and choose from the results to get directions to the trailhead. You can also save any trails that you've hiked and enjoyed or want to hike some other time so you can pull them up easily in the future.
+        Trailblaze is a location based trail-finding application. Plug in how
+        far you're willing to travel and how many miles you'd like to hike and
+        choose from the results to get directions to the trailhead. You can also
+        save any trails that you've hiked and enjoyed or want to hike some other
+        time so you can pull them up easily in the future.
       </Description>
+      <AboutImage
+        src={process.env.PUBLIC_URL + "/chris-rosiak-wMloxuYrjt0-unsplash.jpg"}
+        alt=""
+      />
     </AboutContainer>
   );
 }
